432b81c8f59bf769bbb1bd16f04937ae9d3e75c4
mrfrangipane <mr@frangipane.me>
2020-07-17 18:32:51 +0200

    fix(main): ip when on wifi router

--- oscdmx/__main__.py
+++ oscdmx/__main__.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     if platform.architecture()[0] == '64bit':
         main(samplerate=44100, tablet_ip='192.168.0.10')  # Desktop: Fireface / Freebox router
     else:
-        main(samplerate=48000, tablet_ip='192.168.1.102')  # Raspberry: PiSound / Wifi router
+        main(samplerate=48000, tablet_ip='192.168.1.101')  # Raspberry: PiSound / Wifi router
